37ab6c8329fd6de8e529f639aee7b74a7a231e53
Bart Ouwerkerk <bart26nl@gmail.com>
2024-01-04 09:53:46 +0100

    Add new BrerPublisher tests and safety checks
    Introduced a new `BrerPublisherTest.cs` with comprehensive unit tests. Also enhanced `BrerPublisher.cs` with safety checks to validate input parameters. This helps to ensure proper handling of both `null` and empty topic or message values.

---
+++ Brer/Brer.Tests/Publisher/BrerPublisherTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Brer.Core;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+using Brer.Publisher;
+using Brer.Core.Interfaces;
+using BrerTests.Helpers;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace BrerTests.Publisher
+{
+    public class BrerPublisherTest
+    {
+        private readonly IBrerContext _context = Substitute.For<IBrerContext>();
+        private readonly IModel _model = Substitute.For<IModel>();
+        private readonly ILogger<IBrerContext> _logger;
+        private readonly string _exchangeName = "ex";
+        private readonly BrerPublisher _sut;
+
+        public BrerPublisherTest()
+        {
+            _context.Connection.CreateModel().Returns(_model);
+            _context.BrerOptions.Returns(new BrerOptions(Substitute.For<IConnectionFactory>(), _exchangeName, "q"));
+            _logger = Substitute.For<MockLogger<IBrerContext>>();
+            _context.Logger.Returns(_logger);
+            _sut = new BrerPublisher(_context);
+        }
+
+        [Fact]
+        public void Publish_ShouldCall_CreateModel_And_ExchangeDeclare_When_Called()
+        {
+            // Arrange
+            
+            var topic = "topic";
+            var message = "message";
+            
+            var properties = Substitute.For<IBasicProperties>();
+            properties.ContentType.Returns(topic);
+
+            _model.CreateBasicProperties().Returns(properties);
+
+            // Act
+            _sut.Publish(topic, message);
+
+            // Assert
+            _context.Connection.Received(1).CreateModel();
+            _model.Received(1).ExchangeDeclare(_context.BrerOptions.ExchangeName, ExchangeType.Topic);
+        }
+
+        [Fact]
+        public void Publish_ShouldLogInformation_When_Called()
+        {
+            // Arrange
+            var topic = "topic";
+            var message = "message";
+
+            // Act
+            _sut.Publish(topic, message);
+
+            // Assert
+            _context.Logger.Received(1).LogInformation(
+                "Publishing event of topic {topic} with object {message} to exchange {exchangeName}",
+                topic, message, _context.BrerOptions.ExchangeName);
+        }
+
+        [Fact]
+        public void Publish_ShouldPublish_When_Called()
+        {
+            // Arrange
+            var topic = "topic";
+            var message = "message";
+            var body = Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(message));
+        
+            // Act
+            _sut.Publish(topic, message);
+        
+            // Assert
+            _model.Received(1).BasicPublish(
+                exchange: _context.BrerOptions.ExchangeName,
+                routingKey: topic,
+                mandatory: false,
+                basicProperties: Arg.Any<IBasicProperties>(),
+                Arg.Is<ReadOnlyMemory<byte>>(r => body.SequenceEqual(r.ToArray())));
+        }
+        
+        [Fact]
+        public void Publish_ShouldThrowException_When_Topic_Is_Null()
+        {
+            // Arrange
+            string topic = null;
+            var message = "message";
+        
+            // Act
+            Action act = () => _sut.Publish(topic, message);
+        
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+        
+        
+        [Fact]
+        public void Publish_ShouldThrowException_When_Topic_Is_EmptyString()
+        {
+            // Arrange
+            var topic = "";
+            var message = "message";
+        
+            // Act
+            Action act = () => _sut.Publish(topic, message);
+        
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void Publish_ShouldThrowException_When_Topic_Is_WhiteSpaceString()
+        {
+            // Arrange
+            var topic = "   ";
+            var message = "message";
+        
+            // Act
+            Action act = () => _sut.Publish(topic, message);
+        
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void Publish_ShouldThrowException_When_Object_Is_Null()
+        {
+            // Arrange
+            var topic = "topic";
+            string message = null;
+        
+            // Act
+            Action act = () => _sut.Publish(topic, message);
+        
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
--- Brer/Brer/Publisher/BrerPublisher.cs
+++ Brer/Brer/Publisher/BrerPublisher.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using Brer.Core.Interfaces;
 using Brer.Publisher.Interfaces;
 using Microsoft.Extensions.Logging;
@@ -18,6 +19,10 @@ internal class BrerPublisher : IBrerPublisher
 
     public void Publish<T>(string topic, T obj)
     {
+        // caller safety we don't need to throw given type specification. more a safety net for developers.
+        if (string.IsNullOrEmpty(topic) || string.IsNullOrWhiteSpace(topic)) throw new ArgumentNullException(nameof(topic)); 
+        if (obj == null) throw new ArgumentNullException(nameof(obj));
+        
         using IModel channel = _context.Connection.CreateModel();
         channel.ExchangeDeclare(
             exchange: _context.BrerOptions.ExchangeName,
@@ -25,7 +30,7 @@ internal class BrerPublisher : IBrerPublisher
         );
 
         _context.Logger.LogInformation("Publishing event of topic {top} with object {tobj} to exchange {ex}",
-            topic, obj?.ToString(), _context.BrerOptions.ExchangeName);
+            topic, obj.ToString(), _context.BrerOptions.ExchangeName);
 
         byte[] body = Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(obj));
 
